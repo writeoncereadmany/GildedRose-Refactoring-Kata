d3a6becdb9285f2353c81892830cd063a8290a5a
tom.johnson <tom.johnson@transficc.com>
2021-09-17 13:23:13 +0100

    Make the Java project more self-contained, and provide a junit approach for asserting and approving

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,37 +1,58 @@
 package com.gildedrose;
 
-public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.readString;
+import static java.nio.file.Files.writeString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TexttestFixture {
+
+    public static final Path EXPECTED_OUTPUT = Paths.get("src/test/resources/expected.out");
+
+    public static final Item[] ITEMS = new Item[]{
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        new Item("Conjured Mana Cake", 3, 6)};
+
+    @Test
+    public void outputMatchesReference() throws Exception
+    {
+        assertEquals(readString(EXPECTED_OUTPUT), generateOutput(2, ITEMS));
+    }
+
+    @Disabled("Run this manually to update the expected output based on current behaviour")
+    @Test
+    public void approveCurrentOutput() throws Exception
+    {
+        writeString(EXPECTED_OUTPUT, generateOutput(2, ITEMS));
+    }
+
+    public static String generateOutput(int days, Item[] items) {
+        String output = "OMGHAI!\n";
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            output += "-------- day " + i + " --------\n";
+            output += "name, sellIn, quality\n";
             for (Item item : items) {
-                System.out.println(item);
+                output += item + "\n";
             }
-            System.out.println();
+            output += "\n";
             app.updateQuality();
         }
+        return output;
     }
-
 }
